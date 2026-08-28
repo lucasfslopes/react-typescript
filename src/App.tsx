6d33702a5cb4daefdedaccd9a57421a37b39e9e3
lucasfslopes <lukasmdf2@gmail.com>
2026-08-28 00:07:46 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const [count, setCount] = useState(0)
+
+  return (
+
+    <>
+      <p>Hello World</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Count {count}
+      </button>
+    </>
+  )
+}
+
+export default App
